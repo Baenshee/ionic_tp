017eb1a66ce6845052ec74599e4f5e72ae3c534f
Baenshee <mathieu-valero@outlook.com>
2017-10-26 16:52:23 +0200

    Saving imageData

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -17,6 +17,7 @@ import { Base64ToGallery } from '@ionic-native/base64-to-gallery';
 })
 export class TestPage {
   public base64Image: string;
+  public imageData: string;
   constructor(public navCtrl: NavController,
               public camera: Camera,
               private base64ToGallery: Base64ToGallery) {
@@ -30,13 +31,14 @@ export class TestPage {
     }).then((imageData) => {
       // imageData is a base64 encoded string
       this.base64Image = "data:image/jpeg;base64," + imageData;
+      this.imageData=imageData;
     }, (err) => {
       console.log(err);
     });
   }
 
   savePicture(){
-    this.base64ToGallery.base64ToGallery(this.base64Image, { prefix: '_img' }).then(
+    this.base64ToGallery.base64ToGallery(this.imageData, { prefix: '_img' }).then(
       res => console.log('Saved image to gallery ', res),
       err => console.log('Error saving image to gallery ', err)
     );
