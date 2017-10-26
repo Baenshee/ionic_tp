5de4ac980c20cdcf6fda29b8bd01d3b1135ac346
Baenshee <mathieu-valero@outlook.com>
2017-10-26 14:59:49 +0200

    Implement Camera ?

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public base64Image: string;
+  app: any = { name: String, version: Number};
+  constructor(public navCtrl: NavController, public camera: Camera) {
+    this.app.name = "AppName";
+    this.app.version = "0.0.1";
+  }
 
-  constructor(public navCtrl: NavController) {
-
+  takePicture(){
+    this.camera.getPicture({
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
   }
 
 }
