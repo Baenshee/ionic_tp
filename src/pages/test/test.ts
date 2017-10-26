de059d2a72ebe84c5adba7398c886ea7de7fccda
Baenshee <mathieu-valero@outlook.com>
2017-10-26 16:26:42 +0200

    Camera in second page

---
+++ src/pages/test/test.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+
+/**
+ * Generated class for the TestPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-test',
+  templateUrl: 'test.html',
+})
+export class TestPage {
+  public base64Image: string;
+  constructor(public navCtrl: NavController, public camera: Camera) {
+  }
+
+  takePicture(){
+    this.camera.getPicture({
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+}
